4dd38d71fef1d078bf1b71c9d35638e010f1dd56
winkywong1998 <winkywong1998@gmail.com>
2023-03-31 12:43:05 -0400

    Fix the test in ReservationTest

--- src/test/java/com/jpmc/theater/ReservationTests.java
+++ src/test/java/com/jpmc/theater/ReservationTests.java
@@ -3,7 +3,9 @@ package com.jpmc.theater;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,11 +14,9 @@ public class ReservationTests {
     @Test
     void totalFee() {
         var customer = new Customer("John Doe", "unused-id");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", "Dummy Description",Duration.ofMinutes(90), 12.5, 1),
-                1,
-                LocalDateTime.now()
-        );
-        assertTrue(new Reservation(customer, showing, 3).totalFee() == 37.5);
+        Movie spiderMan = new Movie("Spider-Man: No Way Home","Dummy Description", Duration.ofMinutes(90),12.5, 0);
+        //Only discount applied is the 1st sequence of the day discount. 12.5 - 3 = 9.5
+        Showing showing = new Showing(spiderMan, 1, LocalDateTime.of(LocalDate.of(2023,3, 30), LocalTime.of(10,0)));
+        assertTrue(new Reservation(customer, showing, 3).totalFee() == 9.5 * 3);
     }
 }
