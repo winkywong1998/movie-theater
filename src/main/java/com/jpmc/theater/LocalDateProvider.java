0cefd2cbddcfaf81dda2adef1295083e8bf8fb6f
winkywong1998 <winkywong1998@gmail.com>
2023-03-31 12:03:37 -0400

    Fix the LocalDateProvider

--- src/main/java/com/jpmc/theater/LocalDateProvider.java
+++ src/main/java/com/jpmc/theater/LocalDateProvider.java
@@ -3,17 +3,25 @@ package com.jpmc.theater;
 import java.time.LocalDate;
 
 public class LocalDateProvider {
-    private static LocalDateProvider instance = null;
+    // Add volatile to ensure visibility of changes across threads
+    private volatile static LocalDateProvider instance = null;
+
+    private LocalDateProvider() {}
 
     /**
      * @return make sure to return singleton instance
      */
     public static LocalDateProvider singleton() {
         if (instance == null) {
-            instance = new LocalDateProvider();
+            // Make it thread safe
+            synchronized (LocalDateProvider.class) {
+                if (instance == null) {
+                    instance = new LocalDateProvider();
+                }
+            }
         }
-            return instance;
-        }
+        return instance;
+    }
 
     public LocalDate currentDate() {
             return LocalDate.now();
