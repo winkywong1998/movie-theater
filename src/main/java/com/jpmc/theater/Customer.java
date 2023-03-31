a2b6063b902c8e885cfd7b44525a73e6cab2120d
winkywong1998 <winkywong1998@gmail.com>
2023-03-31 12:59:03 -0400

    Reformat and doc Customer

--- src/main/java/com/jpmc/theater/Customer.java
+++ src/main/java/com/jpmc/theater/Customer.java
@@ -3,12 +3,11 @@ package com.jpmc.theater;
 import java.util.Objects;
 
 public class Customer {
-
     private String name;
-
     private String id;
-
     /**
+     * Constructs a new customer with the given name and ID.
+     *
      * @param name customer name
      * @param id customer id
      */
@@ -17,8 +16,14 @@ public class Customer {
 
         this.name = name;
 
-        }
+    }
 
+    /**
+     * Compares this customer to another object for equality.
+     *
+     * @param o the object to compare to
+     * @return  true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -27,11 +32,21 @@ public class Customer {
         return Objects.equals(name, customer.name) && Objects.equals(id, customer.id);
     }
 
+    /**
+     * Returns a hash code for this customer.
+     *
+     * @return the hash code
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, id);
     }
 
+    /**
+     * Returns a string representation of this customer by name only.
+     *
+     * @return the string representation
+     */
     @Override
     public String toString() {
         return "name: " + name;
